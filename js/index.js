7805500a2fb350a0e2163e657860999c87860ed7
cashlalala <cash924n@gmail.com>
2018-07-16 01:22:10 +0800

    use the message icon of font-awesome

--- js/index.js
+++ js/index.js
@@ -4,16 +4,20 @@ require('jquery-ui/themes/base/button.css');
 require('jquery-ui/themes/base/dialog.css');
 require('jquery-ui/themes/base/resizable.css');
 require('jquery-ui/themes/base/theme.css');
+require('./node_modules/font-awesome/css/font-awesome.css');
 
 var jq = require('jquery');
 
 jq(document).on('click', function (event) {
 
     try {
-        console.log(event.target.id);
+
+        // console.log(event.target.id);
         var eventId = event.target.id;
-        if (eventId && eventId.startsWith("link_")) {
-            var id = "#promptMsg_" + eventId.split("_")[1];
+        if (eventId && (eventId.startsWith("link_") || eventId.startsWith("icon_"))) {
+            var buildId = eventId.split("_")[1];
+            console.log(buildId);
+            var id = "#promptMsg_" + buildId;
             console.log(id);
             var dlg = jq(id).dialog({
                 autoOpen: false,
@@ -28,7 +32,7 @@ jq(document).on('click', function (event) {
             dlg.dialog("open");
         }
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 
 });
